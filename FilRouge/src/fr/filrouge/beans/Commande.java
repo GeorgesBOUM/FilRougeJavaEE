a1479e8557210aff63c0d56ed40a7086e571e27b
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-11 09:43:50 +0100

    bean commande --> ajout getters et setters

--- FilRouge/src/fr/filrouge/beans/Commande.java
+++ FilRouge/src/fr/filrouge/beans/Commande.java
@@ -17,6 +17,53 @@ public class Commande {
 		this.statutLivraison = statutLivraison;
 		this.statutCommande = statutCommande;
 	}
-	
+
+	public Client getClient() {
+		return client;
+	}
+
+	public double getMontant() {
+		return montant;
+	}
+
+	public String getModePaiement() {
+		return modePaiement;
+	}
+
+	public String getModeLivraison() {
+		return modeLivraison;
+	}
+
+	public boolean isStatutLivraison() {
+		return statutLivraison;
+	}
+
+	public boolean isStatutCommande() {
+		return statutCommande;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+
+	public void setModePaiement(String modePaiement) {
+		this.modePaiement = modePaiement;
+	}
+
+	public void setModeLivraison(String modeLivraison) {
+		this.modeLivraison = modeLivraison;
+	}
+
+	public void setStatutLivraison(boolean statutLivraison) {
+		this.statutLivraison = statutLivraison;
+	}
+
+	public void setStatutCommande(boolean statutCommande) {
+		this.statutCommande = statutCommande;
+	}
 	
 }
