dec5f05e4dc0ad427cc1c0b9fee02865ad02fba3
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-11 09:26:34 +0100

    bean --> création du modèle de client

---
+++ FilRouge/src/fr/filrouge/beans/Client.java
@@ -0,0 +1,9 @@
+package fr.filrouge.beans;
+
+public class Client {
+	private String nom;
+	private String prenom;
+	private String adresseLivraison;
+	private String telephone;
+	private String mailString;
+}
