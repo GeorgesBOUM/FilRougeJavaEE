caaba2f1fb25b83a450e4cb7299315416de87f3e
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-11 09:34:23 +0100

    bean client --> ajout constructeur

--- FilRouge/src/fr/filrouge/beans/Client.java
+++ FilRouge/src/fr/filrouge/beans/Client.java
@@ -5,5 +5,14 @@ public class Client {
 	private String prenom;
 	private String adresseLivraison;
 	private String telephone;
-	private String mailString;
+	private String mail;
+	
+	public Client(String nom, String prenom, String adresseLivraison, String telephone, String mail) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresseLivraison = adresseLivraison;
+		this.telephone = telephone;
+		this.mail = mail;
+	}
+
 }
