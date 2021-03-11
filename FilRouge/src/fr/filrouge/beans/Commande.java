b6ede624d047752655dad02fb2d7e593f233c969
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-11 09:39:47 +0100

    bean --> création du modèle commande

---
+++ FilRouge/src/fr/filrouge/beans/Commande.java
@@ -0,0 +1,10 @@
+package fr.filrouge.beans;
+
+public class Commande {
+	Client client;
+	double montant;
+	String modePaiement;
+	String modeLivraison;
+	boolean statutLivraison;
+	boolean statutCommande;
+}
