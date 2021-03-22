b10e01bd058f538d49c56b6420de73a444a88b5d
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-22 15:39:23 +0100

    servlet CreationClient: mise en place de constantes

--- FilRouge/src/fr/filrouge/servlets/CreationClient.java
+++ FilRouge/src/fr/filrouge/servlets/CreationClient.java
@@ -15,6 +15,23 @@ import fr.filrouge.beans.Client;
 @WebServlet("/CreationClient")
 public class CreationClient extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	public static final String CHAMP_NOM       = "nomClient";
+    public static final String CHAMP_PRENOM    = "prenomClient";
+    public static final String CHAMP_ADRESSE   = "adresseClient";
+    public static final String CHAMP_TELEPHONE = "telephoneClient";
+    public static final String CHAMP_EMAIL     = "emailClient";
+    
+    public static final String ATTRIBUT_NOM       = "nom";
+    public static final String ATTRIBUT_PRENOM    = "prenom";
+    public static final String ATTRIBUT_ADRESSE   = "adresse";
+    public static final String ATTRIBUT_TELEPHONE = "telephone";
+    public static final String ATTRIBUT_EMAIL     = "email";
+ 
+    public static final String ATTRIBUT_CLIENT      = "client";
+    public static final String ATTRIBUT_MESSAGE     = "message";
+ 
+    public static final String VUE             = "/AffichageInfoClient.jsp";
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -38,27 +55,27 @@ public class CreationClient extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String nom = request.getParameter("nomClient");
-		String prenom = request.getParameter("prenomClient");
-		String adresse = request.getParameter("adresseClient");
-		String telephone = request.getParameter("telephoneClient");
-		String mail = request.getParameter("emailClient");
+		String nom = request.getParameter(CHAMP_NOM);
+		String prenom = request.getParameter(CHAMP_PRENOM);
+		String adresse = request.getParameter(CHAMP_ADRESSE);
+		String telephone = request.getParameter(CHAMP_TELEPHONE);
+		String mail = request.getParameter(CHAMP_EMAIL);
 		String message = "";
 		
 		if (nom.trim().isEmpty() || adresse.trim().isEmpty() || telephone.trim().isEmpty()) {
 			message = "Erreur - Vous n'avez pas rempli correctement tous les champs obligatoires. <br/>";
-			request.setAttribute("message", message);
-			request.setAttribute("nom", nom);
-			request.setAttribute("prenom", prenom);
-			request.setAttribute("adresse", adresse);
-			request.setAttribute("telephone", telephone);
-			request.setAttribute("mail", mail);
-			request.getRequestDispatcher("/FormulaireCreationClient.jsp").forward(request, response);
+			request.setAttribute(ATTRIBUT_MESSAGE, message);
+			request.setAttribute(ATTRIBUT_NOM, nom);
+			request.setAttribute(ATTRIBUT_PRENOM, prenom);
+			request.setAttribute(ATTRIBUT_ADRESSE, adresse);
+			request.setAttribute(ATTRIBUT_TELEPHONE, telephone);
+			request.setAttribute(ATTRIBUT_EMAIL, mail);
+			request.getRequestDispatcher(VUE).forward(request, response);
 		} else {
 			message = "Client créé avec succès";
 			Client client = new Client(nom, prenom, adresse, telephone, mail);
-			request.setAttribute("message", message);
-			request.setAttribute("client", client);
+			request.setAttribute(ATTRIBUT_MESSAGE, message);
+			request.setAttribute(ATTRIBUT_CLIENT, client);
 			request.getRequestDispatcher("/AffichageInfoClient.jsp").forward(request, response);
 		}
 //		doGet(request, response);
