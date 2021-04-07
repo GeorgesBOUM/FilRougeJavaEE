17061d637da4826bb023b7501978ec50fe84c95f
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-07 05:42:19 +0200

    ajout des champs spécifiques à la commande

--- FilRouge/src/fr/filrouge/validations/ValidationCommande.java
+++ FilRouge/src/fr/filrouge/validations/ValidationCommande.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 public class ValidationCommande {
 	
+	private static final String CHAMP_DATE             = "dateCommande";
+    private static final String CHAMP_MONTANT          = "montantCommande";
+    private static final String CHAMP_MODE_PAIEMENT    = "modePaiementCommande";
+    private static final String CHAMP_STATUT_PAIEMENT  = "statutPaiementCommande";
+    private static final String CHAMP_MODE_LIVRAISON   = "modeLivraisonCommande";
+    private static final String CHAMP_STATUT_LIVRAISON = "statutLivraisonCommande";
+	
 	private String              resultat;
     private Map<String, String> erreurs      = new HashMap<String, String>();
     
