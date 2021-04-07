887906ab8595e339d173937880b8ee3a4a2d36aa
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-07 06:13:04 +0200

    Récupération des champs

--- FilRouge/src/fr/filrouge/validations/ValidationCommande.java
+++ FilRouge/src/fr/filrouge/validations/ValidationCommande.java
@@ -3,6 +3,10 @@ package fr.filrouge.validations;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import fr.filrouge.beans.Commande;
+
 public class ValidationCommande {
 	
 	private static final String CHAMP_DATE             = "dateCommande";
@@ -11,6 +15,8 @@ public class ValidationCommande {
     private static final String CHAMP_STATUT_PAIEMENT  = "statutPaiementCommande";
     private static final String CHAMP_MODE_LIVRAISON   = "modeLivraisonCommande";
     private static final String CHAMP_STATUT_LIVRAISON = "statutLivraisonCommande";
+    
+    private static final String FORMAT_DATE            = "dd/MM/yyyy HH:mm:ss";
 	
 	private String              resultat;
     private Map<String, String> erreurs      = new HashMap<String, String>();
@@ -22,4 +28,34 @@ public class ValidationCommande {
     public Map<String, String> getErreurs() {
 		return erreurs;
 	}
+    
+    public Commande validerNouvelleCommande (HttpServletRequest request) {
+		
+//    	DateTime dt = new DateTime();
+//        DateTimeFormatter formatter = DateTimeFormat.forPattern( FORMAT_DATE );
+//        String date = dt.toString( formatter );
+
+        String montant = getValeurChamp( request, CHAMP_MONTANT );
+        String modePaiement = getValeurChamp( request, CHAMP_MODE_PAIEMENT );
+        String statutPaiement = getValeurChamp( request, CHAMP_STATUT_PAIEMENT );
+        String modeLivraison = getValeurChamp( request, CHAMP_MODE_LIVRAISON );
+        String statutLivraison = getValeurChamp( request, CHAMP_STATUT_LIVRAISON );
+        
+    	Commande commande = new Commande();
+    	
+    	
+    	
+    	return commande;
+	}
+    
+    private String getValeurChamp( HttpServletRequest request, String nomChamp ) {
+        String valeur = request.getParameter( nomChamp );
+        if ( valeur == null || valeur.trim().length() == 0 ) {
+            return null;
+        } else {
+            return valeur.trim();
+        }
+    }
+    
+    
 }
